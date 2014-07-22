10ea4995b660523a4bc724a2f6d5fefb4cd30657
Josh Schramm <josh.schramm@gmail.com>
2014-07-22 10:55:30 -0400

    Changed rails app check to skip all other types of application

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,5 +1,8 @@
-if node[:opsworks][:instance][:layers].include?('rails-app')
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping opsworks_custom_config::write_config #{application} as it is not a rails app")
+    next
+  end
 
   include_recipe "opsworks_custom_config::write_config"
-
 end
