37a1cd79fc5cdfdf35cebb8540e97dbbeebf8723
Josh Schramm <josh.schramm@gmail.com>
2014-07-23 15:40:40 -0400

    argh

--- definitions/custom_config_template.rb
+++ definitions/custom_config_template.rb
@@ -6,19 +6,19 @@
 
 define :custom_config_template do
   Chef::Log.debug("Applying custom config entries for #{params[:application]}")
-  Chef::Log.debug(params.inspect)
-  return if params[:custom_config].nil? || params[:application].nil? || params[:custom_config][params[:application]].nil?
 
-  params[:custom_config][params[:application]].each do |file_name, config|
-    template "#{params[:deploy][:deploy_to]}/shared/config/#{file_name}.yml" do
-      source "sample.yml.erb"
-      owner params[:deploy][:user]
-      group params[:deploy][:group]
-      mode "0660"
-      variables :env => config, :rails_env => params[:deploy][:rails_env]
+  if params[:custom_config] && params[:application] && params[:custom_config][params[:application]]
+    params[:custom_config][params[:application]].each do |file_name, config|
+      template "#{params[:deploy][:deploy_to]}/shared/config/#{file_name}.yml" do
+        source "sample.yml.erb"
+        owner params[:deploy][:user]
+        group params[:deploy][:group]
+        mode "0660"
+        variables :env => config, :rails_env => params[:deploy][:rails_env]
 
-      only_if do
-        File.exists?("#{params[:deploy][:deploy_to]}/shared/config")
+        only_if do
+          File.exists?("#{params[:deploy][:deploy_to]}/shared/config")
+        end
       end
     end
   end
